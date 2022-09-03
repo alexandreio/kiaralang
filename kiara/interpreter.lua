88cddbcf06f15a949093265db86d78b27ff83d6f
Alexandre Ferreira <alexandrewho@icloud.com>
2022-09-03 20:18:21 -0300

    refactor: update interpreter to run inside a function

--- kiara/interpreter.lua
+++ kiara/interpreter.lua
@@ -6,28 +6,47 @@ local backend_build = require("kiara.backend.build")
 local params = { ... }
 
 
-if params[1] then
-    local file = io.open(params[1], "r")
-    io.input(file)
-    input = io.read("a")
-    io.close(file)
-else
-    input = io.read()
+local function run(params, debug)
+    local input = nil
+    if params[1] then
+        local file = io.open(params[1], "r")
+        io.input(file)
+        input = io.read("a")
+        io.close(file)
+    else
+        input = io.read()
+    end
+
+    if debug == true then
+        print("input:", input)
+        print("")
+    end
+    -- 
+    -- 
+    local ast = parser.parse(input)
+    if debug == true then
+        print("ast:")
+        print(pt.pt(ast))
+    end
+    
+    
+    local code = compiler:compile(ast)
+    if debug == true then
+        print("\ncode:")
+        print(pt.pt(code))
+    end
+
+    
+    local stack = {}
+    local mem = { k0 = 0, k1 = 1, k10 = 10 }
+    backend_build.run(code, mem, stack, 0)
+    if debug == true then
+        print("\nstack:")
+        print(stack[1])
+    end
+
+    return stack[1]
 end
 
 
-print("input:", input)
-print("")
-local ast = parser.parse(input)
-print("ast:")
-print(pt.pt(ast))
-
-local code = compiler:compile(ast)
-print("\ncode:")
-print(pt.pt(code))
-
-local stack = {}
-local mem = { k0 = 0, k1 = 1, k10 = 10 }
-backend_build.run(code, mem, stack, 0)
-print("\nstack:")
-print(stack[1])
+run(params, debug)
